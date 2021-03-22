d9f9a901be849d78d11f5073ea3030f019fac01c
takeru.hashimoto <hstake25@gmail.com>
2021-03-22 23:38:27 +0900

    ログ出力追加

--- terraform/ecs_nginx/main.tf
+++ terraform/ecs_nginx/main.tf
@@ -46,3 +46,8 @@ resource "aws_lb_listener_rule" "rule" {
     }
   }
 }
+
+resource "aws_cloudwatch_log_group" "for_ecs" {
+  name              = "/ecs/example/${local.name}"
+  retention_in_days = 180
+}
