b794cf637500521ddabdf09272b746b8e549fe9f
takeru.hashimoto <hstake25@gmail.com>
2021-03-23 17:31:11 +0900

    ecs security group に https インバウンドルール追加

--- terraform/ecs_nginx/main.tf
+++ terraform/ecs_nginx/main.tf
@@ -71,12 +71,22 @@ resource "aws_security_group" "ecs_security_group" {
   }
 }
 
-resource "aws_security_group_rule" "ingress_rule" {
+resource "aws_security_group_rule" "http_ingress_rule" {
   security_group_id = aws_security_group.ecs_security_group.id
   type              = "ingress"
 
   from_port   = 80
   to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "https_ingress_rule" {
+  security_group_id = aws_security_group.ecs_security_group.id
+  type              = "ingress"
+
+  from_port   = 443
+  to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 }
