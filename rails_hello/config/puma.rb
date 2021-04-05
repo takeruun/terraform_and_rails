45486248c44c6ce0c325f01489d5dab6caa51407
hashimoto <t-hashimoto@anysense.co.jp>
2021-04-05 10:18:15 +0900

    puma sockets file

--- rails_hello/config/puma.rb
+++ rails_hello/config/puma.rb
@@ -39,5 +39,8 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 #
 # preload_app!
 
+app_root = File.expand_path("../..", __FILE__)
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
+
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
